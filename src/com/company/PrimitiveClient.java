d314bf6dae201b555ccf72b26f04c40539957eac
rozsabence <robence@gmail.com>
2019-09-16 13:08:24 +0200

    log time spent between client and server

--- src/com/company/PrimitiveClient.java
+++ src/com/company/PrimitiveClient.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 public class PrimitiveClient {
@@ -11,6 +13,8 @@ public class PrimitiveClient {
         final String HOST = "127.0.0.1";
         final int PORT = 12345;
         final BigDecimal SIZE = new BigDecimal(1000000);
+
+        LocalDateTime start = LocalDateTime.now();
 
         try (
                 var s = new Socket(HOST, PORT);
@@ -33,6 +37,14 @@ public class PrimitiveClient {
                 String msg2 = sc.nextLine();
                 System.out.println(msg2);
             }
+
+            LocalDateTime now = LocalDateTime.now();
+            long millis = ChronoUnit.MILLIS.between(start, now);
+
+            System.out.println("Size: " + SIZE);
+            System.out.println("Start: " + start);
+            System.out.println("Now " + now);
+            System.out.println("Time: " + millis + " millis");
 
         } catch (IOException e) {
             e.printStackTrace();
