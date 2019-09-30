5792388e832cb28ffa5d1c67b983adb2ace28017
rozsabence <robence@gmail.com>
2019-09-30 12:33:02 +0200

    add ever listening server

--- src/com/company/PrimitiveServer.java
+++ src/com/company/PrimitiveServer.java
@@ -10,41 +10,24 @@ public class PrimitiveServer {
     private static final int SERVER_PORT = 12345;
 
     public static void main(String[] args) {
+
+        int clientCount = 0;
+
         try (
                 ServerSocket ss = new ServerSocket(SERVER_PORT);
-                Socket s = ss.accept();
-                Scanner sc = new Scanner(s.getInputStream());
-                PrintWriter pw = new PrintWriter(s.getOutputStream());
         ) {
-            System.out.println("Listening on port: " + SERVER_PORT);
-            pw.flush();
-            int length = Integer.parseInt(sc.nextLine());
-
-            String line;
-            System.out.println("Receiving");
-            for (int i = 0; i < length; i ++) {
-                line = sc.nextLine();
-                int number = Integer.parseInt(line);
-                System.out.println("Received: " + number);
-                int calculatedNumber = calc(number);
-                String data = String.valueOf(calculatedNumber);
-                System.out.println("Sending: " + data);
-                System.out.println("---");
-                pw.println(data);
+            while (true) {
+                try (Socket s = ss.accept();
+                     Scanner sc = new Scanner(s.getInputStream());
+                     PrintWriter pw = new PrintWriter(s.getOutputStream());
+                ) {
+                    ++clientCount;
+                    pw.println(clientCount);
+                    pw.flush();
+                }
             }
-            pw.flush();
-
-            System.out.println("Flushed");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static int calc(int number) {
-        if (number == 0) {
-            return 0;
-        }
-        return number * 2 + 1;
     }
 }
