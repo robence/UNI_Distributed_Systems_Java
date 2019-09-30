3d58f53ec081d39ab25e31b76f1feb52617697c5
rozsabence <robence@gmail.com>
2019-09-30 12:44:00 +0200

    print file content from server

--- src/com/company/PrimitiveServer.java
+++ src/com/company/PrimitiveServer.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -21,9 +22,24 @@ public class PrimitiveServer {
                      Scanner sc = new Scanner(s.getInputStream());
                      PrintWriter pw = new PrintWriter(s.getOutputStream());
                 ) {
-                    ++clientCount;
-                    pw.println(clientCount);
-                    pw.flush();
+
+                    while (sc.hasNextLine()) {
+                        var filename = sc.next();
+
+                        var file = new File(filename);
+                        if(file.exists()) {
+                            try(var scFile = new Scanner(file)) {
+                                while (scFile.hasNextLine()) {
+                                    var line = scFile.nextLine();
+                                    pw.println(line);
+                                }
+                            }
+                        } else {
+                            pw.println("Missing file " + filename);
+                        }
+                        pw.flush();
+
+                    }
                 }
             }
         } catch (IOException e) {
