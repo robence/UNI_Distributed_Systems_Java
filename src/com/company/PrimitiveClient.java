ef0bc61baf781b77681549ffebe5eb5c246ca6fa
rozsabence <robence@gmail.com>
2019-10-07 13:26:01 +0200

    add client communication from app

--- src/com/company/PrimitiveClient.java
+++ src/com/company/PrimitiveClient.java
@@ -1,52 +1,45 @@
 package com.company;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Scanner;
 
-import static com.company.IOHelper.readFromServer;
-import static com.company.TimeLogger.logTime;
-
 public class PrimitiveClient {
-    private static final String HOST = "127.0.0.1";
-    private static final int PORT = 12345;
-    private static final String OUTPUT_FILE_PATH = "resources/sum.txt";
+    private static final int CLIENT_PORT = 12345;
 
     public static void main(String[] args) {
+
         try (
-                var socket = new Socket(HOST, PORT);
-                var pwInput = new PrintWriter(socket.getOutputStream());
-                var scInput = new Scanner(socket.getInputStream());
-                var fileWriter = new FileWriter(OUTPUT_FILE_PATH);
-                var pwOutput = new PrintWriter(fileWriter)) {
-            LocalDateTime startedAt = LocalDateTime.now();
-            int sumOfAllReceivedNumbers = 0;
+                var s = new Socket("localhost", CLIENT_PORT);
+                var sc = new Scanner(s.getInputStream());
+                var pw = new PrintWriter(s.getOutputStream());
 
-            List<String> portsFromServer = readFromServer(scInput);
-            for (String port : portsFromServer) {
-                final int PORT_FROM_SERVER = Integer.parseInt(port);
+                var scIn = new Scanner(System.in);
+                var pwOut = new PrintWriter(System.out);
+        ) {
 
-                try (
-                        var dynamicSocket = new Socket(HOST, PORT_FROM_SERVER);
-                        var sc = new Scanner(dynamicSocket.getInputStream());
+            sendMsg(scIn, pw);
 
-                ) {
-                    while (sc.hasNextInt()) {
-                        int currentNum = sc.nextInt();
-                        sumOfAllReceivedNumbers += currentNum;
-                        pwOutput.println(sumOfAllReceivedNumbers);
-                    }
-                }
-
+            if(args.length > 0) {
+                sendMsg(sc, pwOut);
             }
-            // log elapsed time in millis
-            logTime(startedAt);
-        } catch (IOException e) {
+
+            while (true) {
+                if (!sendMsg(scIn, pw)) break;
+                if (!sendMsg(sc, pwOut)) break;
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    private static boolean sendMsg(Scanner sc, PrintWriter pw) {
+        if (!sc.hasNextLine()) return false;
+        String line = sc.nextLine();
+
+        pw.println(line);
+        pw.flush();
+        return true;
+    }
 }
+
